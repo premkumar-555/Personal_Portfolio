7c0f2b69faccbb28bd670391e86df01c3b39ce07
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-03-07 15:56:30 +0530

    added side navigation bar

--- js/app.js
+++ js/app.js
@@ -8,3 +8,13 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
   });
 });
+let hamberger = document.querySelector(".hamburger");
+let times = document.querySelector(".times");
+let mobileNav = document.querySelector(".mobile-nav");
+hamberger.addEventListener("click", () => {
+  console.log("yes");
+  mobileNav.classList.add("open");
+});
+times.addEventListener("click", () => {
+  mobileNav.classList.remove("open");
+});
